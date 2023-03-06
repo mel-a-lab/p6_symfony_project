5ad0cb2f953cae9f02500d48c1a90d422866279d
mel-a-lab <melanie.dussenne@gmail.com>
2023-03-06 17:04:32 +0100

    description contraint NotBlank

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Trick;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TrickType extends AbstractType
 {
@@ -14,6 +16,14 @@ class TrickType extends AbstractType
         $builder
             ->add('name') // mettre type chaine de caractères
             // ajouter les propriétés simple (description avec contrainte et le groupe, ajouter contraintes dans formulaire, faire afficher erreur sur front, on peut typer juste après le champ , c'est le cas pour le groupe)
+            ->add('description', TextareaType::class, [
+                'label' => 'Contenu de l\'article',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ "Description" ne doit pas être vide.'
+                    ])
+                ]
+            ]);
         ;
     }
 
