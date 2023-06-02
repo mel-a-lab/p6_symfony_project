87f3e58864e3252a5f61020440a4758002e3bee0
mel-a-lab <melanie.dussenne@gmail.com>
2023-06-02 11:31:23 +0200

    modif HomeController.com

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,45 +16,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', 'home.index', methods: ['GET'])]
-    public function index(TrickRepository $trickRepository) : Response
+    public function index(TrickRepository $trickRepository): Response
     {
         //$tricks = ['figure1','figure2','figure3','figure4','figure5'];
         $tricks = $trickRepository->findAll();
-        return $this->render('home.html.twig',[
-            'tricks'=>$tricks
+        return $this->render('home.html.twig', [
+            'tricks' => $tricks
         ]);
     }
 
     #[Route('/signin', 'signin', methods: ['GET'])]
-    public function signin() : Response
+    public function signin(): Response
     {
         return $this->render('signin.html.twig');
     }
 
     #[Route('/signup', 'signup', methods: ['GET'])]
-    public function signup() : Response
+    public function signup(): Response
     {
         return $this->render('signup.html.twig');
     }
-    
+
+
     #[Route('/email')]
-    
-   public function sendMail(MailerInterface $mailer, Request $request )
-   {
-      // ...
+    public function sendMail(MailerInterface $mailer, Request $request)
+    {
+        // ...
 
-      $mail = (new TemplatedEmail())
-      ->from(new Address('expediteur@demo.test', 'Mon nom'))
-      ->to('melanie.dussenne@gmail.com')
-      ->subject('Mon beau sujet')
-      ->htmlTemplate('mail/template.html.twig')
-      ->context([
-        'firstname' => 'Joe'
-     ])
-   ;
-   
-   $mailer->send($mail);
-   
+        $mail = (new TemplatedEmail())
+            ->from(new Address('expediteur@demo.test', 'Mon nom'))
+            ->to('melanie.dussenne@gmail.com')
+            ->subject('Mon beau sujet')
+            ->htmlTemplate('mail/template.html.twig')
+            ->context([
+                'firstname' => 'Joe'
+            ])
+        ;
 
-}
+        $mailer->send($mail);
+
+
+    }
 }
